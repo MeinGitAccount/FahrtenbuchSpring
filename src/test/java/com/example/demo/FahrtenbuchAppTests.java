a875389cd61bda8dd56e8990dda6fad18717101c
jakob <jakob.som@gmx.at>
2023-11-11 16:38:05 +0100

    Junit Test Fahrt.java

--- src/test/java/com/example/demo/FahrtenbuchAppTests.java
+++ src/test/java/com/example/demo/FahrtenbuchAppTests.java
@@ -1,10 +1,20 @@
 package com.example.demo;
 
+import com.example.demo.enums.Status;
+import com.example.demo.enums.Wiederholung;
+import com.example.demo.model.Fahrt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @SpringBootTest
 class FahrtenbuchAppTests {
+
 
     @Test
     void contextLoads() {
